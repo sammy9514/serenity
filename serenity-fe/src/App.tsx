19b9f67ffbd15d9b4d74e9df4fc2d9d9f7612992
Ayomide <ogunyemiayomide700@gmail.com>
2026-09-15 23:32:02 +0100

    feat: header with nav and book now btn

--- serenity-fe/src/App.tsx
+++ serenity-fe/src/App.tsx
@@ -1,5 +1,12 @@
+import Header from "./Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full min-h-screen  ">
+      <Header />
+      <div className=" mx-auto max-w-page px-6 "></div>
+    </div>
+  );
 };
 
 export default App;
