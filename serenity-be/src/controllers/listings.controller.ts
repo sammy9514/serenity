108349666831bc9cd25c191c97084ec5d1ab0cda
Ayomide <ogunyemiayomide700@gmail.com>
2026-09-20 23:56:03 +0100

    fix(server) slug lint error

--- serenity-be/src/controllers/listings.controller.ts
+++ serenity-be/src/controllers/listings.controller.ts
@@ -33,11 +33,11 @@ export const getAvailability = async (req: Request, res: Response) => {
       .json({ error: { code: "BAD_REQUEST", message: "invalid date range" } });
   }
   const { slug } = req.params;
-  const listing = await Listing.findOne({ slug });
   if (!slug)
     return res
       .status(400)
       .json({ error: { code: "BAD_REQUEST", message: "slug is required" } });
+  const listing = await Listing.findOne({ slug });
 
   if (!listing)
     return res
